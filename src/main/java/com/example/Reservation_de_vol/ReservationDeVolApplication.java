aa0bf5f835becbd4eea8cdb63066dcd4505e5ced
phasna <phasnaaun69@gmail.com>
2025-04-04 13:45:38 +0200

    phasna

---
+++ src/main/java/com/example/Reservation_de_vol/ReservationDeVolApplication.java
@@ -0,0 +1,13 @@
+package com.example.Reservation_de_vol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationDeVolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationDeVolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Reservation_de_vol/ReservationDeVolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Reservation_de_vol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationDeVolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
